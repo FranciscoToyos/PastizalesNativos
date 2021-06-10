4cc2011a34a2aa8a7b6e422470a664cb47118f4b
FranciscoToyos <Francabj95@gmail.com>
2021-06-10 16:21:59 -0300

    Se termino el formulario

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,5 +9,8 @@ router.get('/', function(req, res, next) {
 router.get('/saludo', function(req, res, next) {
   res.render('saludo',{ title: 'Pastizales Nativos' });
 });
-
+/* GET form page. */
+router.get('/formulario', function(req, res, next) {
+  res.render('formulario',{ title: 'Pastizales Nativos' });
+});
 module.exports = router;
